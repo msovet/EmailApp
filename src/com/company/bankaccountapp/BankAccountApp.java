5e40593456ee07fbfe85a389334ed5f95b54f59f
msovet <merei9822@gmail.com>
2021-08-24 10:23:16 +0600

    csv reader

--- src/com/company/bankaccountapp/BankAccountApp.java
+++ src/com/company/bankaccountapp/BankAccountApp.java
@@ -1,11 +1,13 @@
 package com.company.bankaccountapp;
 
+import com.company.utilities.CSV;
+
 import java.util.*;
 
 public class BankAccountApp {
     public static void main(String[] args) {
-        Savings sAcc = new Savings("Sagat", "123456789", 5000);
-        Checking cAcc = new Checking("Bolat", "123123123", 3000);
+//        Savings sAcc = new Savings("Sagat", "123456789", 5000);
+//        Checking cAcc = new Checking("Bolat", "123123123", 3000);
 //        Child ch = new Child("Child" , "12312311", 9000);
 
 
@@ -14,11 +16,21 @@ public class BankAccountApp {
 //        System.out.println("**************");
 //        cAcc.showInfo();
 //        ch.showInfo();
-        sAcc.printBalance();
-        sAcc.deposit(800);
-        sAcc.withdraw(300);
-        sAcc.transfer("Mereyge", 2000);
-        System.out.println("*******************");
-        sAcc.compound();
+//        sAcc.printBalance();
+//        sAcc.deposit(800);
+//        sAcc.withdraw(300);
+//        sAcc.transfer("Mereyge", 2000);
+//        System.out.println("*******************");
+//        sAcc.compound();
+
+        // Read CSV and create new accounts
+        String file = "C:\\Users\\msovet\\Desktop\\courseDoc\\NewBankAccounts.csv";
+        List<String[]> newCustomers = CSV.read(file);
+        for (String [] newCustomer: newCustomers) {
+            System.out.print(newCustomer[0]);
+            System.out.print(newCustomer[1]);
+            System.out.print(newCustomer[2]);
+            System.out.println(newCustomer[3]);
+        }
     }
 }
---
+++ src/com/company/utilities/CSV.java
@@ -0,0 +1,24 @@
+package com.company.utilities;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CSV {
+    // read data from CSV and return list
+    public static List<String[]> read(String file) {
+        List<String[]> data = new LinkedList<String[]>();
+        String dataRow;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while ((dataRow = br.readLine()) != null) {
+                String[] dataRecords = dataRow.split(",");
+                data.add(dataRecords);
+            }
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return data;
+    }
+}
